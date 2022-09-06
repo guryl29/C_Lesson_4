f1fad0e235e2d3831b273de7c76a3bd17ac61532
guryl29 <111236089+guryl29@users.noreply.github.com>
2022-09-06 10:03:34 +0300

    HW

---
+++ number25/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+    
+Console.WriteLine("Vvedite chislo");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Vvedite chislo");
+int b = int.Parse(Console.ReadLine());
+
+double res = Math.Pow(a, b);
+Console.WriteLine(res);
---
+++ number27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Vvedite chislo: ");
+int a= int.Parse(Console.ReadLine());
+int sum = 0;
+int num=0;
+while( a > 0)
+    {
+        num = a%10;
+        sum=sum+num;
+        a=a/10;
+
+    }
+
+Console.WriteLine(sum);
